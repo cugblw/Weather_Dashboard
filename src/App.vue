9a9c2a83de45d0082495f4217c5e82ea0b141e15
DELL <cugblw2014@outlook.com>
2020-08-05 17:54:32 +0800

    weather dashboard

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <p>This is component's code is in {{filename}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        filename: 'App.vue'
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+  }
+</script>
+
+<style>
+
+</style>
