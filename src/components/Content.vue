1423b4b0637eba6e7671a3728529c37296a9d1aa
DELL <cugblw2014@outlook.com>
2020-08-05 18:15:12 +0800

    childComponents

---
+++ src/components/Content.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <p>This child components of Content.vue are:</p>
+        <ul>
+            <li v-for="child in chindComponents">{{child}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                childComponents: ['TempVarChart.vue', 'Hightlights.vue']
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+
+        }
+    }
+</script>
+
+<style>
+
+</style>
