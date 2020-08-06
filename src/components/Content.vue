645d7468c9264dc0cb4ef68d62f026173c0d9393
Weil Lee <cugblw2014@outlook.com>
2020-08-06 17:21:05 +0800

    ...

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,17 +1,19 @@
 <template>
-    <div>
+    <div id="pagecontent">
         <p>This child components of Content.vue are:</p>
         <ul>
-            <li v-for="child in chindComponents">{{child}}</li>
+            <li v-for="child in childComponents">{{child}}</li>
         </ul>
+        {{weather_data}}
     </div>
 </template>
 
 <script>
     export default {
+        props: ["weather_data"],
         data() {
             return {
-                childComponents: ['TempVarChart.vue', 'Hightlights.vue']
+                childComponents: ['TempVarChart.vue', 'Highlights.vue', 'UVIndex.vue']
             }
         },
         methods: {
@@ -19,10 +21,13 @@
         },
         computed: {
 
-        }
+        },
     }
 </script>
 
 <style>
-
+    #pagecontent {
+        border: 1px solid black;
+        padding: 2px;
+    }
 </style>
